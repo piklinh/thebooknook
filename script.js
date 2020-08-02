633333b0d9ac653bce859ccbffb26913be449244
Bart Yellapu <38868384+bartmann88@users.noreply.github.com>
2020-08-02 11:46:45 -0500

    fucntionlaity addition for hide dom data

--- script.js
+++ script.js
@@ -24,53 +24,53 @@ const seriesBooks = "Series Books";
 
 // // Create a generic api calling function called getList which will accept 1 parameter which is type -  it will get the data which is passed from parameter 
 
-bookApp.getList = function(type) {
+bookApp.getList = function (type) {
 
-        $.ajax({
-            url:'https://api.nytimes.com/svc/books/v3/lists?api-key=bMG46sv2E3bGGar6zGp9djz6GRhQy78B',
-            method: 'GET',
-            dataType: 'json',
-            data: {
-                ////// TO-DO: Find out how to get multiple lists and display when radio button clicked - solved 
-                list: `${type}`, 
-                // "picture-books"
-            }
-                }).then(function(result) {
+    $.ajax({
+        url: 'https://api.nytimes.com/svc/books/v3/lists?api-key=bMG46sv2E3bGGar6zGp9djz6GRhQy78B',
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            ////// TO-DO: Find out how to get multiple lists and display when radio button clicked - solved 
+            list: `${type}`,
+            // "picture-books"
+        }
+    }).then(function (result) {
 
-                //     // console.log(result.results);
-                //     const res = result.results;
-                //     res.forEach((el) => {
-                //         console.log(el);
-                //     })
-                //     $.map(result, function () {
-                //         $('.bookList').append(`
-                //     <h2>${result.results[0].book_details[0].title}</h2>
-                //     <p>${result.results[0].book_details[0].author}</p>
-                //     <p>${result.results[0].book_details[0].description}</p>
-                //     <a>${result.results[0].amazon_product_url}</a>
-                //     <p>${result.results[0].rank}</p>
-                // `);
+            // console.log(result.results);
+            const res = result.results;
+            res.forEach((el) => {
+                // consolel
+                // $.map(result, function () {
+                    $('#bookList').append(`
+                    <p>${el.rank}</p>
+                    <h2>${el.book_details[0].title}</h2>
+                    <p>${el.book_details[0].author}</p>
+                    <p>${el.book_details[0].description}</p>
+                    <a>${el.amazon_product_url}</a>
+                    `);
+                })
 
-                ///// TO DO: Need some kind of loop here to get each result
-                
-                // TEST CODE: console.log(result.results[0].book_details[0].title);
-                    // bookApp.getFormatData(result)
+        ///// TO DO: Need some kind of loop here to get each result
 
-                let showResults = result.results.map(val => (val))
-                // console.log( showResults);
-                let amazonUrl =  showResults.map(val => (val.amazon_product_url))
-                console.log(amazonUrl);
-                let authorName = showResults.map(val => (val.book_details[0].author))
-                // console.log(authorName);
-                let title =  showResults.map(val => (val.book_details[0].title))
-                // console.log(title)
-                let bookDescription = showResults.map(val => (val.book_details[0].description))
-                // console.log(bookDescription);
-                let rank = showResults.map(val => val.rank)
-                // console.log (rank)
-                
-                });
-    };
+        // TEST CODE: console.log(result.results[0].book_details[0].title);
+        // bookApp.getFormatData(result)
+
+        // let showResults = result.results.map(val => (val))
+        // // console.log( showResults);
+        // let amazonUrl = showResults.map(val => (val.amazon_product_url))
+        // console.log(amazonUrl);
+        // let authorName = showResults.map(val => (val.book_details[0].author))
+        // // console.log(authorName);
+        // let title = showResults.map(val => (val.book_details[0].title))
+        // // console.log(title)
+        // let bookDescription = showResults.map(val => (val.book_details[0].description))
+        // // console.log(bookDescription);
+        // let rank = showResults.map(val => val.rank)
+        // // console.log (rank)
+
+    });
+};
 
 
 
@@ -78,22 +78,25 @@ bookApp.getList = function(type) {
 //     // function for all radio buttons - Will pass type on radio button selection - the type will be passed through get list function - b
 
     bookApp.radioButtonChangeHandler = function (type) {
-        bookApp.getList(type)
+        bookApp.getList(type);
     }
 
 //     // changehandler function of radio button to push the each type variable -b
     
     $('#pictureBook').on('change', function () {
+        $('#bookList').html("")
         bookApp.radioButtonChangeHandler(pictureBooks);
     });
     
     $('#childrenBook').on('change', function () {
+        $(".bookList").html("")
 
     bookApp.radioButtonChangeHandler(childrenMiddleGrade)
         
     });
     
     $('#seriesBook').on('change', function () {
+        $(".bookList").html("")
         bookApp.radioButtonChangeHandler(seriesBooks)
     });
 
@@ -104,25 +107,11 @@ bookApp.getList = function(type) {
 // ////// TO DO: Need to connect the value to the result from the AJAX API
 // ////// TO DO: To display one default list first
 
-//     // On change, grab the selected item from input 
-//     // $('input').on('change', function(event) {
-//     //     const grabItem = $(this).val();
-//     //     console.log(grabItem);
-//     // });
 
 //     /////// To append book details with either forEach() or map()
 
-
-
 //     // 
 //     //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $.each to result to iterate through array  -  create template literals use this function<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-//     // const ranking = 
-//     // const title = 
-//     // const author =
-//     // const description =
-//     // const purchaseLink = 
-// }
 
 // // To initialize the app
 bookApp.init = function() {
@@ -130,9 +119,6 @@ bookApp.init = function() {
     bookApp.getList();
     // bookApp.displayList();
 }
-
-
-
 
 // Document ready
 $(function() {
