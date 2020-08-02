545137d41bf44f04a32d50f8ee29da982cba9692
piklin <hplesson@gmail.com>
2020-08-01 23:16:43 -0400

    book lists display properly :raised_hands::tada::books:

--- script.js
+++ script.js
@@ -7,21 +7,11 @@ bookApp.scroll = function(event) {
             scrollTop: $('main').offset().top}, 1000);
     });
 };
- 
-
-// Type parameter will return data based on given variable 
-
-// variables for book category created for type 
-const pictureBooks = "picture-books";
-const childrenMiddleGrade = "Childrens Middle Grade Hardcover";
-const seriesBooks = "Series Books";
-
 
 // TO DO: Is there a way to use this instead of listing it on the URL
 bookApp.key = "bMG46sv2E3bGGar6zGp9djz6GRhQy78B"
 
 // Gets book list from the third-party API using AJAX request 
-// 
 
 // bookApp.getFormatData = function(data){
 //     data.forEach(result => {
@@ -29,60 +19,59 @@ bookApp.key = "bMG46sv2E3bGGar6zGp9djz6GRhQy78B"
 // }
 
 
+// Type parameter will return data based on given variable 
+// Create variables for each book category that was created specifically for type 
+const pictureBooks = "picture-books";
+const childrenMiddleGrade = "childrens-middle-grade";
+const seriesBooks = "series-books";
+
 // Create a generic api calling function called getList which will accept 1 parameter which is type -  it will get the data which is passed from parameter 
 
 bookApp.getList = function(type) {
 
-
         $.ajax({
-            url:'https://api.nytimes.com/svc/books/v3/lists?api-key=bMG46sv2E3bGGar6zGp9djz6GRhQy78B',
-            method: 'GET',
-            dataType: 'json',
-            data: {
-                ////// TO-DO: Find out how to get multiple lists and display when radio button clicked - solved 
-                list: `${type}`, 
+        url:'https://api.nytimes.com/svc/books/v3/lists?api-key=bMG46sv2E3bGGar6zGp9djz6GRhQy78B',
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            list: `${type}`, 
             }
-                }).then(function(result) {
-                ///// TO DO: Need some kind of loop here to get each result
-                
-                // TEST CODE: console.log(result.results[0].book_details[0].title);
-                    // bookApp.getFormatData(result)
-                    console.log(result); 
-                });
 
-    };
+        // Once the Ajax request is resolved successfully, use .then method to return a value that is stored in the result parameter
+        }).then(function(result) {
+            
+            // The result parameter initially contains an object, and the information that we needed is in an array called results 
+            // Created a variable called res to store the array
+            const res = result.results;
+            console.log(result.results);
 
-    // function for all radio buttons - Will pass type on radio button selection - the type will be passed through get list function - b
+            // Use the forEach method to loop through the array
+            // It contains a callback function with each element of the array
+            // The elements of the array are stored into the parameter of el
+            res.forEach((el) =>{
+                console.log(el);
 
-    bookApp.radioButtonChangeHandler = function (type) {
-        bookApp.getList(type)
-    }
+                // The data is then appended to the div of booklist
+                // TO-DO: To append the items properly into the HTML (e.g. button)
+                    $('.bookList').append(`
+                        <div>
+                            <p>${el.rank}</p>
+                            <h2>${el.book_details[0].title}</h2>
+                            <p>${el.book_details[0].author}</p>
+                            <p>Book Description: ${el.book_details[0].description}</p>
+                            <button>${el.amazon_product_url}</button>   
+                        </div>
+                    `);
 
-bookApp.displayList = function() {
+                })
 
-// To grab the value from the radio button
-////// TO DO: Need to connect the value to the result from the AJAX API
-////// TO DO: To display one default list first
-
-    // On change, grab the selected item from input 
-    $('input').on('change', function(event) {
-        const grabItem = $(this).val();
-        console.log(grabItem);
-    });
-
-    /////// To append book details with either forEach() or map()
-    // const ranking = 
-    // const title = 
-    // const author =
-    // const description =
-    // const purchaseLink = 
+            })
 }
 
-// To initialize the app
-bookApp.init = function() {
-    bookApp.scroll();
-    bookApp.getList();
-    bookApp.displayList();
+// function for all radio buttons - Will pass type on radio button selection - the type will be passed through get list function - b
+
+bookApp.radioButtonChangeHandler = function (type) {
+    bookApp.getList(type)
 }
 
 // changehandler function of radio button to push the each type variable -b
@@ -92,14 +81,42 @@ $('#pictureBook').on('change', function () {
 });
 
 $('#childrenBook').on('change', function () {
-    bookApp.radioButtonChangeHandler(childrenMiddleGrade)
+    const childrenBookRes = bookApp.radioButtonChangeHandler(childrenMiddleGrade)
 });
 
 $('#seriesBook').on('change', function () {
-    bookApp.radioButtonChangeHandler(seriesBooks)
+    const seriesBookRes = bookApp.radioButtonChangeHandler(seriesBooks)
 });
 
+// TO-DO: Figure out a way to hide/empty/detach the information that is not needed in the app
+// TO-DO: Leaving the commented out area below as we try to figure out how to resolve this
 
+// When User clicked on the input of 'bookCategory', the input that is checked will return a value
+// The value will be stored in a variable called radioValue
+
+// $("input[name='bookCategory']").click(function() {
+//     const radioValue = $("input[name='bookCategory']:checked").val();
+
+// // If radioValue is pictureBook, it should hide/empty/detach the results of the others
+//     if (radioValue === 'pictureBook') {
+//         console.log('This is picture book');
+//         $('childrenBookRes').hide();
+//         $('seriesBookRes').hide();
+//     } else if (radioValue === 'childrenBook') {
+//         $('pictureBookRes').hide();
+//         $('seriesBookRes').hide();
+//     } else (radioValue === 'seriesBook')
+//         $('pictureBookRes').hide();
+//         $('childrenBookRes').hide();
+// })
+
+
+// To initialize the app
+bookApp.init = function() {
+    bookApp.scroll();
+    bookApp.getList();
+    // bookApp.displayList();
+}
 
 // Document ready
 $(function() {
