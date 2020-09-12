6c4093ad7deff98251f44ab471936b086ac7e0c1
piklin <hplesson@gmail.com>
2020-09-12 01:43:42 -0400

    done for now, switched to button for a11y, add favicon :sunglasses::sunny::wheelchair:

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ bookApp.getList = (listName) => {
             // The result parameter initially contains an object, and the information that we needed is in an array called results 
             // Created a variable called bookRes to store the array
             const bookRes = res.results.books;
-            console.log(res.results.books);
 
             // Empties the text of the bookList div before forEach runs a loop
             $('.bookList').text("");
@@ -33,7 +32,7 @@ bookApp.getList = (listName) => {
                                 <h3>${el.author}</h3>
                                 <p class="bookDescription">Book Description:</p>
                                 <p class="bookDescription bookDetails">${el.description}</p>
-                                <button><a href="${el.amazon_product_url}" class="productUrl" target="_blank" rel="noopener noreferrer">Get a copy!</a></button>
+                                <a href="${el.amazon_product_url}" class="productUrl" target="_blank" rel="noopener noreferrer">Get a copy!</a>
                             </div>
                         </div>
                     `);
@@ -43,18 +42,17 @@ bookApp.getList = (listName) => {
 
 // Button that listens to input and updates the value
 bookApp.eventListener = () => {
-    $('input').on('change', function() {
+    $('.radioButton').on('click', function() {
+        event.preventDefault();
         const bookInput = $(this).val();
 
         bookApp.getList(bookInput)
-        console.log(bookInput);
     })
 }
 
 // Scroll button for the header to top of page to start reading
 bookApp.scroll = () => {
     $('button').on('click', function() {
-    
         $('html').animate( {
             scrollTop: $('main').offset().top}, 1000);
     });
@@ -71,7 +69,3 @@ bookApp.init = () => {
 $(function() {
     bookApp.init();
 })
-
-// TO DO: Add button in the end, it should appear after the list is generated. Might have to setTimeOut
-// TO DO: Re-style and add accessibility to the buttons
-// TO DO: Error handling for first click / missing book descriptions/book cover
